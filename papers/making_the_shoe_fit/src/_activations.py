6bbb1aa762c9261cd606210d29702daac468ac51
Santiago Hincapie <shpotes3@gmail.com>
2021-12-14 16:25:27 -0500

    add gelu

--- papers/making_the_shoe_fit/src/_activations.py
+++ papers/making_the_shoe_fit/src/_activations.py
@@ -3,3 +3,7 @@ from flax import linen as nn
 class ReLU(nn.Module):
     def __call__(self, x):
         return nn.relu(x)
+
+class GeLU(nn.Module):
+    def __call__(self, x):
+        return nn.gelu(x)
